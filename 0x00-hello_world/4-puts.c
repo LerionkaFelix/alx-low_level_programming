b8d94304a2f39465013be30c3c501955f4663bbc
Felix lerionka <lerionkafelix8@gmail.com>
2023-06-16 03:18:45 -0700

    C changes

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - begining of the code
+ * return: should be 0
+ */
+
+int main ()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}

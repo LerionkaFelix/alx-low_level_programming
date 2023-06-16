2011cebc79a892462d3778ba98bc84419b0ad2b3
Felix lerionka <lerionkafelix8@gmail.com>
2023-06-16 03:29:53 -0700

    C changes

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * description: main - begining of the code 
+ * @void : return value
+ * Return : should be 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

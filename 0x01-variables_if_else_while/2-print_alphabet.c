34fdca907e1ec131e0c79452b097d6bd998b6cd6
Felix lerionka <lerionkafelix8@gmail.com>
2023-06-17 00:56:41 -0700

    all hail alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - beginining of the code
+ * @void: Return  value
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}

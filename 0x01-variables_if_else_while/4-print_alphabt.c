59438b484a0d1e3b9d2c922efd1fff01d4f07127
Felix lerionka <lerionkafelix8@gmail.com>
2023-06-17 01:22:33 -0700

    omiting q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - begigining of the programm
+ * @void : return type
+ * Return : always 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'q' || c == 'e')
+			continue;
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}

b46bd36453986afdf9c3ca7a329e4e97b8f47c10
Felix lerionka <lerionkafelix8@gmail.com>
2023-06-17 01:47:36 -0700

    decimals using putchar '

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - begining of the code
+ * @void: return value
+ * Return: return type
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+
+	return (0);
+}

ef594c7e2e1a89ef863dfa05cd4dc7e78da06f95
Felix lerionka <lerionkafelix8@gmail.com>
2023-06-22 15:40:05 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints characters
+ *
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	int l;
+	char i;
+
+	for (l = 1 ; l <= 10 ; l++)
+	{
+		for (i = 0 ; i <= 14 ; i++)
+		{
+			if (i > 9)
+				_putchar('1');
+			_putchar(i % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}

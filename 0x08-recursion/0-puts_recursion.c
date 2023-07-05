e4510c0d4161dbcbf8d70436dd9aabc07ced0407
Felix lerionka <lerionkafelix8@gmail.com>
2023-07-05 05:26:28 -0700

    Betty style update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,15 +8,16 @@
 */
 void _puts_recursion(char *s)
 {
-    if (*s == '\0')
-    {
-      _putchar ('\n');
-      return;
-    }
+if (*s == '\0')
+{
+_putchar ('\n');
+return;
+}
 
-    if (*s != '\0')
-      _putchar (*s);
+if (*s != '\0')
+_putchar (*s);
 
-    _puts_recursion(s + 1);
+_puts_recursion(s + 1);
+
 }
 

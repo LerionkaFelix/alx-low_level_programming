625c60ed097324e8b3cfda1336934a5aa5021cfc
LerionkaFelix <lerionkafelix8@gmail.com>
2023-08-01 03:20:28 -0700

    Pop head

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,16 +8,17 @@
 
 int pop_listint(listint_t **head)
 {
-	listint_t *tp;
-	int my_data;
+	listint_t *temp;
+	int num;
 
-	if (*head == NULL)
+	if (!head || !*head)
 		return (0);
 
-	tp = *head;
-	*head = tp->next;
-	my_data = tp->n;
-	free(tp);
-	return (my_data);
+	num = (*head)->n;
+	temp = (*head)->next;
+	free(*head);
+	*head = temp;
+
+	return (num);
 }
 

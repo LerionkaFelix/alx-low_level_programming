386341371b9949eafd205bd71cfd3cb5fdc535c6
LerionkaFelix <lerionkafelix8@gmail.com>
2023-08-01 03:12:09 -0700

    Pop head

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,21 +3,22 @@
 /**
  * free_listint2 - frees a listint_t list
  * @head: pointer to the listint_t list to be freed
+ * Return: head node's dat if success, zero otherwise
  */
 
 int pop_listint(listint_t **head)
 {
-	listint_t *tempptr;
-	int headnum;
+        listint_t *tempptr;
+        int headnum;
 
-	if (!head || !*head)
-		return (0);
+        if (!head || !*head)
+                return (0);
 
-	headnum = (*head)->n;
-	tempptr = (*head)->next;
-	free(*head);
-	*head = tempptr;
-	
-	return (headnum);
+        headnum = (*head)->n;
+        tempptr = (*head)->next;
+        free(*head);
+        *head = tempptr;
+
+        return (headnum);
 }
 

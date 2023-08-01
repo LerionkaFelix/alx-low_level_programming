a0d207cb640ec4b74ca91ead21c89431f1c3f813
LerionkaFelix <lerionkafelix8@gmail.com>
2023-08-01 02:25:30 -0700

    freelistint2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_listint2 - Frees a listint_t list.
+ * @head: Address of the first node of a list.
+ **/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp_ptr;
+
+	if (!head) return;
+
+	while (!*head)
+	{
+		tem_ptr = (*head)->next;
+		free(*head);
+		*head = temp_ptr;
+	}
+}
+

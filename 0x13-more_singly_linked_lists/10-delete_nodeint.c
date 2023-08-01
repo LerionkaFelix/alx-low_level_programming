8964f5c61e13e803f43598cf764aa563e4debb65
LerionkaFelix <lerionkafelix8@gmail.com>
2023-08-01 04:13:38 -0700

    delete node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,33 +10,28 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-listint_t *curr_node;
-listint_t *next;
+	unsigned int i;
+	listint_t *current, *next;
 
-unsigned int i;
-
-if (!head || !(*head))
-{
-    return (-1);
-}
-curr_node = (*head);
-if (!index)
-{
-    (*head) = curr_node->next;
-    free(curr_node);
-    return (1);
-}
-for (i = 0; curr_node && i < index - 1; i++)
-{
-    curr_node = curr_node->next;
-}
-if (!curr_node || !(curr_node->next))
-{
-    return (-1);
-}
-next = curr_node->next->next;
-free(curr_node->next);
-curr_node->next = next;
-return (1);
+	if (head == NULL || *head == NULL)
+		return (-1);
+	if (index == 0)
+	{
+		next = (*head)->next;
+		free(*head);
+		*head = next;
+		return (1);
+	}
+	current = *head;
+	for (i = 0; i < index - 1; i++)
+	{
+		if (current->next == NULL)
+			return (-1);
+		current = current->next;
+	}
+	next = current->next;
+	current->next = next->next;
+	free(next);
+	return (1);
 }
 

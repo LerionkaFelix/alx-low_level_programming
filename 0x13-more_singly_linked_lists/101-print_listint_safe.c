5d5abe5fd04c8e206aaed47da6f0eb968c21e37e
LerionkaFelix <lerionkafelix8@gmail.com>
2023-08-01 04:36:49 -0700

    safe print

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+* looped_listint_len - returns the number of distinct nodes in the list
+* @head: listint_t pointer to the head node
+* Return: If not, the number of distinct nodes in the list
+*/
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t num = 0;
+	long int diff;
+
+	while (head)
+	{
+		diff = head - head->next;
+		num++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		if (diff > 0)
+			head = head->next;
+		else
+		{
+			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			break;
+		}
+	}
+
+	return (num);
+}
+

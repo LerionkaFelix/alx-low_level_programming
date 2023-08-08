be67b3d77c9c69a652b9ea33938d5ad20536ac76
LerionkaFelix <lerionkafelix8@gmail.com>
2023-08-08 03:22:37 -0700

    copy-paste success

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,73 +1,62 @@
 #include "main.h"
-#include <stdio.h>
 
+void _close(int fd);
 /**
- * error_file - checks if files can be opened.
- * @src_file: source file.
- * @dest_file: destination file.
- * @argv: arguments array
- * Return: no return
+ * main - copies the content of a file to another file
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 0 if success or exits
  */
-void error_file(int src_file, int dest_file, char *argv[])
-{
-	if (src_file == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (dest_file == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-}
 
-/**
- * main - main program responsible for copying file contents from one file to the other
- * @argc: number of cmdline arguments
- * @argv: arguments array
- * Return: Always 0.
- */
 int main(int argc, char *argv[])
 {
-	int src_file, dest_file, err_close;
-	ssize_t numchars, numwritten;
+	int from_file, to_file, no_read;
 	char buffer[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp src_file dest_file");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	src_file = open(argv[1], O_RDONLY);
-	dest_file = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(src_file, dest_file, argv);
-
-	numchars = 1024;
-	while (numchars == 1024)
+	from_file = open(argv[1], O_RDONLY);
+	if (from_file < 0)
 	{
-		numchars = read(src_file, buffer, 1024);
-		if (numchars == -1)
-			error_file(-1, 0, argv);
-		numwritten = write(dest_file, buffer, numchars);
-		if (numwritten == -1)
-			error_file(0, -1, argv);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
-
-	err_close = close(src_file);
-	if (err_close == -1)
+	to_file = open(argv[2], O_TRUNC | O_CREAT | O_WRONLY, 0664);
+	while ((no_read = read(from_file, buffer, 1024)) > 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src_file);
-		exit(100);
+		if (to_file < 0 || (write(to_file, buffer, no_read) != no_read))
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
 	}
-
-	err_close = close(dest_file);
-	if (err_close == -1)
+	if (no_read < 0)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src_file);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
+	_close(from_file);
+	_close(to_file);
+
 	return (0);
+}
+
+/**
+*_close - closes a file
+*@fd: file descriptor
+*Return: void
+*/
+void _close(int fd)
+{
+	int shut = close(fd);
+
+	if (shut < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd);
+		exit(100);
+	}
 }
 
